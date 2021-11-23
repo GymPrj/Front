43bb3428eb3b34ab9080b58ae555c40c118343e1
kmin-jeong <desk535353@gmail.com>
2021-11-24 00:44:10 +0900

    fix:로컬에서 babel 버전 충돌& eslint 버전 충돌 해결

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -22,21 +22,6 @@ const Register = () => {
   const onClick = async () => {
     console.log('click');
 
-    // const data = {
-    //   email: 'qwe199942@naver.com',
-    //   password: 'qwe123',
-    // };
-
-    // post 테스트
-    // await axios.post('/session/login', data)
-    // .then(function(response){
-    //   console.log(response);
-
-    // })
-    // .catch(function(error){
-    //   console.log(error);
-    // });
-
     // get test
     await axios
       .get('/test')
