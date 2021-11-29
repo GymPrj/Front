a27cbfd9c81d90473a66c015e7db23c089d4ed9e
chaeyeonLee <dmsgp62@gmail.com>
2021-11-29 23:39:03 +0900

    기본 Header, Footer 페이지 (로그인, 회원가입 제외하고 기본 레이아웃)

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Main from '../components/Main';
+import MainPage from '../components/MainPage';
 import Login from '../components/auth/Login';
 import Register from '../components/auth/Register';
+import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Main} />
         <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} />
+        <>
+          <Header />
+          <Route path="/" exact component={MainPage} />
+          <Footer />
+        </>
       </Switch>
     </BrowserRouter>
   );
