73d2b7d0ce5e87452677312f61cad7a43c74c9ff
chaeyeonLee <dmsgp62@gmail.com>
2021-12-22 17:36:45 +0900

    redux-logger 삭제

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-// import { createBrowserHistory } from 'history';
-// import { routerMiddleware } from 'connected-react-router';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 import persistedReducer from './reducers';
@@ -19,25 +17,3 @@ sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
 
 export default { store, persistor };
-
-// export const history = createBrowserHistory();
-// const sagaMiddleware = createSagaMiddleware();
-
-// const initialState = {};
-
-// const middlewares = [sagaMiddleware, routerMiddleware(history)];
-// const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-
-// const composeEnhancer =
-//   process.env.NODE_ENV === 'production' ? compose : devtools;
-
-// const store = createStore(
-//   persistedReducer,
-//   initialState,
-//   composeEnhancer(applyMiddleware(...middlewares)),
-// );
-// sagaMiddleware.run(rootSaga);
-
-// export const persistor = persistStore(store);
-
-// export default store;
