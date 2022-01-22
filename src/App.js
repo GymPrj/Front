f282b6352d7b910cffdb7ba4db129f5fb23aed3b
chaeyeonLee <dmsgp62@gmail.com>
2022-01-22 23:23:57 +0900

    eslint 규칙 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { PersistGate } from 'redux-persist/integration/react';
 import Router from './routes/Router';
-import './assets/css/common.css';
+import './assets/style/common.css';
 import { store, persistor } from './redux/store';
 import { history } from './redux/reducers';
 
---
+++ src/assets/style/theme.js
@@ -0,0 +1,14 @@
+const size = {
+  mobile: '767px',
+  desktop: '768px',
+};
+
+const theme = {
+  desktopWidth: '1280px',
+  mainPurpleColor: '#4748C6',
+  subDarkColor: '#1B1B1B',
+  mobile: `(max-width: ${size.mobile})`,
+  pc: `(min-width: ${size.desktop})`,
+};
+
+export default theme;
